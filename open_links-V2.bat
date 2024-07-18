e92b1f1d78ec3ae03ef82bd7ea8fbee2bc77f2ca
Aniket Khedkar <102348408+devaniketkhedkar@users.noreply.github.com>
2024-07-18 14:56:42 +0530

    Add files via upload

---
+++ open_links-V2.bat
@@ -0,0 +1,17 @@
+@echo off
+
+REM BSE 
+start https://www.bseindia.com/sensex/code/16/
+timeout /t 3
+
+REM NSE
+start https://www.nseindia.com/get-quotes/equity?symbol=BSE
+timeout /t 3
+
+REM Economics Times 
+start https://economictimes.indiatimes.com/?from=mdr
+timeout /t 3
+
+REM Forbs India
+start https://www.forbesindia.com/
+timeout /t 3
